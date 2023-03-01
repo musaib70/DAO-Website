b8328648c2ad29c709a0053b1b05a9c7d8e9100e
Musaib70 <musaibkhan62@yahoo.com>
2023-03-01 16:04:28 +0500

    button update

--- src/components/shared/button.tsx
+++ src/components/shared/button.tsx
@@ -2,7 +2,7 @@ import React, { FC, ReactNode } from 'react'
 
 const Button:FC<{children: React.ReactNode}> = ({children}) => {
   return (
-    <div className='bg-slate-500 hover:bg-slate-600 text-white font-semibold py-4 px-4 text-center text-lg rounded-full  w-36 h-15'>
+    <div className='bg-slate-500 hover:bg-slate-600 duration-300 text-white font-semibold py-3 px-4 text-center text-lg rounded-full w-36 h-15 '>
 
         <button> {children} </button>
 
--- src/components/widgets/hero.tsx
+++ src/components/widgets/hero.tsx
@@ -12,19 +12,21 @@ const hero = () => {
     <section className=' px-2 py-2'>
     <Wrapper>
         
-     <div className='flex items-center'>
+     <div className='flex items-center flex-1 '>
 
     <div>
     <h4 className='flex mx-2 my-2 text-slate-600 font-medium '>Presidential Initiative for Artificial Intelligence and Computing (PIAIC)</h4>
     <h1 className='font-bold text-4xl mx-2 my-4'> Certified Web 3.0 and Metaverse Developer </h1>
-    <p className='flex mx-2 my-2 text-slate-600	text-sm'>A One and Quarter Years Panaverse DAO Earn as you Learn Program Getting Ready for the Next Generation of the Internet</p>
-    <p className='flex mx-2 my-2 text-slate-600	text-sm'>Consolidating Web 3.0, Metaverse, Artificial Intelligence (AI), Cloud, Edge, Ambient Computing/IoT, Network Automation, and Bioinformatics Technologies</p>
+    <p className='flex mx-2 my-2 text-slate-600	text-sm text-justify'>A One and Quarter Years Panaverse DAO Earn as you Learn Program Getting Ready for the Next Generation of the Internet</p>
+    <p className='flex mx-2 my-2 text-slate-600	text-sm text-justify'>Consolidating Web 3.0, Metaverse, Artificial Intelligence (AI), Cloud, Edge, Ambient Computing/IoT, Network Automation, and Bioinformatics Technologies</p>
     <div className=' mt-6'>
     <Button> Learn More </Button>
+    {/* <br />
+    <Button> Click me </Button> */}
     </div>
     </div>
     
-    <div className=''>
+    <div className='mx-4 my-2'>
             <Image src = {web}
                     alt = "loading...."
                     height={700}
